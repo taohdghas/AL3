a4380b21700fc45518fc825c748d9e144a28f321
taohdghas <143993457+taohdghas@users.noreply.github.com>
2024-06-27 12:28:23 +0900

    02_10確認課題完了

--- DirectXGame/Enemy.h
+++ DirectXGame/Enemy.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "Model.h"
 #include "Vector3.h"
+#include "aabb.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
 /// <summary>
 /// 敵
 /// </summary>
+class Player;
 class Enemy {
 public:
 	Enemy();
@@ -17,7 +19,20 @@ public:
 
 	void Draw();
 
+	//衝突応答
+	void OnCollision(const Player* player);
+
+	// ワールド座標を取得
+	Vector3 GetWorldPosition();
+
+	//AABB取得関数
+	AABB GetAABB();
+
 private:
+	// 敵の当たり判定サイズ
+	static inline const float kWidth = 1.0f;
+	static inline const float kHeight = 1.0f;
+
 	// 補講の速さ
 	static inline const float kWalkSpeed = 0.03f;
 	//最初の角度[度]
