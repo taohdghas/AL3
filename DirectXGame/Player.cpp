b45f699392b2eec25613e7246f3f1845da5b35b3
taohdghas <143993457+taohdghas@users.noreply.github.com>
2024-08-17 19:23:52 +0900

    作業途中

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -102,6 +102,7 @@ void Player::Update(const ViewProjection& viewProjection) {
 	worldTransform3DReticle_.UpdateMatrix();
 
 	//3Dレティクルのワールド座標から2Dレティクルのスクリーン座標を計算
+	
 	Vector3 positionReticle;
 	positionReticle.x = worldTransform3DReticle_.matWorld_.m[3][0];
 	positionReticle.y = worldTransform3DReticle_.matWorld_.m[3][1];
@@ -115,9 +116,40 @@ void Player::Update(const ViewProjection& viewProjection) {
 	//スプライトのレティクルに座標設定
 	sprite2dReticle_->SetPosition(Vector2(positionReticle.x, positionReticle.y));
 
+	POINT mousePosition;
+	// マウス座標(スクリーン座標)を取得する
+	GetCursorPos(&mousePosition);
+	// クライアントエリア座標に変換する
+	HWND hwnd = WinApp::GetInstance()->GetHwnd();
+	ScreenToClient(hwnd, &mousePosition);
+	// マウス座標を2Dレティクルのスプライトに代入する
+	sprite2dReticle_->SetPosition(Vector2(float(mousePosition.x), float(mousePosition.y)));
+	// ビュー行列、射影行列、ビューポート行列の合成行列を計算する
+	Matrix4x4 matVPV = Multiply(Multiply(viewProjection.matView, viewProjection.matProjection), matViewport);
+	// 合成行列の逆行列を計算する
+	Matrix4x4 matInverseVPV = Inverse(matVPV);
+	//スクリーン座標
+	Vector3 posNear = Vector3(float(mousePosition.x), float(mousePosition.y), 0);
+	Vector3 posFar = Vector3(float (mousePosition.x), float(mousePosition.y), 1);
+    //スクリーン座標系からワールド座標系へ
+	posNear = Transform(posNear, matInverseVPV);
+	posFar = Transform(posFar, matInverseVPV);
+	//マウスレイの方向
+	Vector3 mouseDirection = posFar - posNear;
+	mouseDirection = Normalize(mouseDirection);
+	//カメラから照準オブジェクトの距離
+	const float kDistanceTestObject = 100.0f;
+	worldTransform3DReticle_.translation_ = posNear + mouseDirection * kDistanceTestObject;
+	worldTransform3DReticle_.UpdateMatrix();
+
 	// キャラクターの座標を画面表示する処理
 	ImGui::Begin("Debug");
 	ImGui::DragFloat3("Position", &worldTransform_.translation_.x, 0.1f);
+	ImGui::Text("2DReticle:(%f,%f)", positionReticle.x, positionReticle.y);
+	ImGui::Text("Near:(%+.2f,%+.2f,%+.2f)", posNear.x, posNear.y, posNear.z);
+	ImGui::Text("Far:(%+.2f,%+.2f,%+.2f)", posFar.x, posFar.y, posFar.z);
+	ImGui::Text("3DReticle:(%+.2f,%+.2f,%+.2f)", worldTransform3DReticle_.translation_.x, 
+		worldTransform3DReticle_.translation_.y, worldTransform3DReticle_.translation_.z);
 	ImGui::End();
 }
 
@@ -157,7 +189,7 @@ void Player::Attack() {
 		velocity = Normalize(velocity) * kBulletSpeed;
 
 		//速度ベクトルを自機の向きに合わせて回転させる
-	     velocity = TransformNormal(velocity,worldTransform_.matWorld_);
+	   //  velocity = TransformNormal(velocity,worldTransform_.matWorld_);
 
 		// 弾を生成し、初期化
 		PlayerBullet* newBullet = new PlayerBullet();
